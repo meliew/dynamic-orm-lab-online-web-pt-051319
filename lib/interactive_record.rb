4a550d96fe35bbd76a63634790e65a88c430229b
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 17:50:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,4 +19,10 @@ column_names << row["name"]
 end
 column_names.compact
 end
+
+def initialize(options={})
+  options.each do |property, value|
+    self.send("#{property}=", value)
+    end
+  end
 end
