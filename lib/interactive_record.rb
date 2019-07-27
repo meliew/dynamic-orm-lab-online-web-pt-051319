c9814be0df792d333aa9b4f3876a04d6745bf34b
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 17:57:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,9 @@ def self.find_by_name(name)
   DB[:conn].execute(sql, name)
 end
 
-def self.find_by(attribute) 
+def self.find_by(attribute)
+  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+  DB[:conn].execute(sql, name)
 end
 
 
