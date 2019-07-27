c9797a17efd5eea930c0fb076f117c279f306f58
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 18:01:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,6 +54,12 @@ def self.find_by(attribute)
 end
 
 def save
-end 
+  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+  VALUES (#{values_for_insert})"
+
+  DB[:conn].execute(sql)
+
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+end
 
 end
